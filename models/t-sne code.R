13293edc3db511995a35fcf5b358370b6daaf031
omarkhn444 <32711891+omarkhn444@users.noreply.github.com>
2017-11-23 13:30:32 +0000

    Add files via upload

---
+++ models/t-sne code.R
@@ -0,0 +1,27 @@
+install.packages("Rtsne")
+install.packages("rgl")
+library(rgl)
+library(Rtsne)
+#2D
+train<- read.csv("train.csv") 
+class(train$label)
+
+Labels<-train$label
+train$label<-as.factor(train$label)
+
+colours = rainbow(length(unique(train$label)))
+names(colours) = unique(train$label)
+
+tsne <- Rtsne(train[,-1], dims = 2, perplexity=30, verbose=TRUE, max_iter = 500)
+exeTimeTsne<- system.time(Rtsne(train[,-1], dims = 2, perplexity=30, verbose=TRUE, max_iter = 500))
+
+plot(tsne$Y, t='n', main="tsne")
+text(tsne$Y, labels=train$label, col=colours[train$label])
+
+train2D<-cbind(train[,1:784],tsne$Y,train[,785])
+
+#3D
+tsne3d <- Rtsne(train[,-1], dims = 3, perplexity=30, verbose=TRUE, max_iter = 500)
+plot3d(tsne3d$Y, col=colours[train$label])
+
+
