e4d11c1d5c67fd0890bc578050c829a25107b5c9
omarkhn444 <32711891+omarkhn444@users.noreply.github.com>
2017-12-20 11:51:29 +0000

    Add files via upload

--- models/t-sne code.R
+++ models/t-sne code.R
@@ -20,7 +20,16 @@ text(tsne$Y, labels=train$label, col=colours[train$label])
 train2D<-cbind(train[,1:784],tsne$Y,train[,785])
 
 #3D
-tsne3d <- Rtsne(train[,-1], dims = 3, perplexity=30, verbose=TRUE, max_iter = 500)
-plot3d(tsne3d$Y, col=colours[train$label])
+tsne3d <- Rtsne(train[,-1], dims = 3, perplexity=35, verbose=TRUE, max_iter = 500)
+plot3d(tsne3d$Y,labels=train$label, col=colours[train$label])
+legend3d("topright", legend = '0':'9',pch=16, col = colours)
+
+train3D<-cbind(train[,1],tsne3d$Y)
+
+write.csv(train2D,"tsne2D.csv", row.names = F)
+write.csv(train3D,"tsne3D.csv", row.names = F)
 
 
+x<-read.csv("tsne2D.csv")
+y<-read.csv("tsne3D.csv")
+x<-x[,785:787]
